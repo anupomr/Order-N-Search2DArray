c4adb6f69cd4a5f03e90f4953a9ae21daf117efb
anupo <anupomr@yahoo.com>
2020-06-30 07:55:08 -0400

    Main Menu is created

--- 2dArray/src/TwoDimenArray.java
+++ 2dArray/src/TwoDimenArray.java
@@ -10,12 +10,13 @@ import java.util.Scanner;
 public class TwoDimenArray {
 
 
-	public static Scanner kbInput = new Scanner(System.in); // static class scope field accessible to all methods in the
+	public static Scanner userInput = new Scanner(System.in); // static class scope field accessible to all methods in the
 															// class
 	public static final String username = "Anupom"; // TODO CODE: change static field to your username
 	public static final String stdNumber = "300853516"; // TODO CODE: change static field to your student number
 
 	public static void main(String[] args) {
+		int mainMenuOption = 0; // declare and init a mainMenu option
 		String latTwoChar = stdNumber.substring(stdNumber.length() - 2);
 		int magicNumber = Integer.parseInt(latTwoChar);
 		int headernumber =0;
@@ -24,8 +25,20 @@ public class TwoDimenArray {
 		headerText();
 		headerBar(headernumber);
 
+		mainMenuOption = mainMenuText();
+		
+		
+		
 	} //End Main
 	
+	
+	public static int mainMenuText() {
+		System.out.println("\n Select a menu option : \n"
+				+"\t1 - Manually create arrays for processing\n"
+				+"\t2 - Automatically create arrays for processing\n");
+		int retValue=userInput.nextInt();
+		return retValue;
+	}
 	/**
 	 * This overloaded method prints between 40 and 60 '*' characters to the console
 	 * as a separate line for formatted output. It defaults to 40 "*" characters if
